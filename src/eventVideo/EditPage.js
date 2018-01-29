233e38e801111a086b9c319335f3e3846c3e8673
chenrui2014 <chenruixiang2009@163.com>
2018-01-30 00:58:55 +0800

    去掉摄像头类型和厂商

--- src/eventVideo/EditPage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import {render} from 'react-dom';
-import Reflv from 'reflv';
-const staticServer = "http://localhost:8088";
-export default EditPage = (data)=>(<Reflv url={staticServer+data.path} type='flv'> </Reflv>);
